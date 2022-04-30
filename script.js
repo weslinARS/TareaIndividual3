55be5832546e435f3a04cdb641269ad9ff520398
DESKTOP-79VDJDE\Weslin <weslinsilva2017@gmail.com>
2022-04-30 15:42:04 -0600

    terminado

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function calcularEdad() {
-  var edadHumana = parseInt(document.getElementById("edadHumana").value);
+  var edad;
+  edad =parseInt( document.getElementById("EdadH").value);
   var resultado;
-  document.getElementById('edadCanina').innerHTML = edadHumana
-  resultado = edadHumana * 7;
-  var calculo = `Su mascota tiene ${resultado} años en edad canina`;
-  alert(resultado);
+  resultado = edad * 7;
+  var calculo = `Su mascota tiene <span> ${resultado} </span> años en edad canina`;
   document.getElementById('edadCanina').innerHTML = calculo;
 }
